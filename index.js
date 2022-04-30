7048e96ffa1ec9326a2d5835d902d20b57b5b930
herohossen <delowarhossen1224@gmail.com>
2022-05-01 03:26:57 +0600

    mongodb added

--- index.js
+++ index.js
@@ -2,17 +2,53 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 8888;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@inventory-management-p1.mpnfu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+console.log(uri);
+async function run() {
+  try {
+    await client.connect();
+    //database name and colletion
+    const userCollection = client
+      .db("inventory-management-p11")
+      .collection("products");
+
+    //get user
+    app.get("/user", async (req, res) => {
+      const query = {};
+      const cursor = userCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+
+      app.get("/user", async (req, res) => {
+        const query = {};
+        const cursor = userCollection.find(query);
+        const users = await cursor.toArray();
+        res.send(users);
+      });
+
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
