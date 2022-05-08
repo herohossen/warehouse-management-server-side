742b048802b5eb61e11ad9acb324c58e3e3be8fe
herohossen <delowarhossen1224@gmail.com>
2022-05-08 18:59:30 +0600

    JWT added

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 8888;
 
+
 // middleware
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+  const authHeader= req.headers.authorization;
+if(!authHeader){
+return res.status(401).send({message: 'Unauthorized access'})
+}
+const token =authHeader.split(' ')[1];
+jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+if(err){
+  return res.status(403).send({message: 'Forbidden Access'})
+}
+console.log('decoded', decoded);
+req.decoded=decoded;
+next();
+})
+  // console.log('inside Jwt',authHeader);
+
+
+}
 
 //Server Connection String
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@inventory-management-p1.mpnfu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -16,8 +37,8 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-console.log(uri);
-
+// console.log(uri);
+//Funtion for api call
 async function run() {
   try {
     await client.connect();
@@ -25,6 +46,17 @@ async function run() {
     const itemCollection = client
       .db("inventory-management-p11")
       .collection("products");
+
+//auth
+app.post('/login', async(req,res)=>{
+const user =req.body;
+const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+  expiresIn:'1d'
+});
+res.send({accessToken});
+})
+
+
 
     //get item
     app.get("/item", async (req, res) => {
@@ -50,6 +82,7 @@ async function run() {
       res.send(result);
     });
 
+    //Get Item by id
     app.get("/item/:id", async (req, res) => {
       const id = req.params.id;
       // console.log(id);
@@ -58,17 +91,17 @@ async function run() {
       res.send(product);
     });
 
+    //Update item Quantity
     app.put("/item/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
-      const updatedProduct = req.body;
-
-      console.log(updatedProduct);
+      // console.log(id);
+      const updatedItem = req.body;
+      console.log(updatedItem);
       const filter = { _id: ObjectId(id) };
       const options = { upsert: true };
       const updatedDoc = {
         $set: {
-          quantity: updatedProduct.quantity,
+          quantity: updatedItem.quantity,
         },
       };
       const result = await itemCollection.updateOne(
@@ -79,21 +112,28 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/useritem", async (req, res) => {
+    //Get api data by email 
+    app.get("/useritem", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
       const email = req.query.email;
-      console.log(email);
+      // console.log(email);
+if(email===decodedEmail){
+  const query = { email: email };
+  const cursor = itemCollection.find(query);
+  const useritems = await cursor.toArray();
+  res.send(useritems);
+}
+else{
+  res.status(403).send({message : 'forbidden access'})
+}
 
-      const query = { email: email };
-      const cursor = itemCollection.find(query);
-      const useritems = await cursor.toArray();
-      res.send(useritems);
     });
 
   } finally {
     // await client.close();
   }
 }
-//run funtion
+//Exicute funtion
 run().catch(console.dir);
 //Server Running ok
 app.get("/", (req, res) => {
